820a32678000eac6bc23710ba982275c94ed0c00
VTrofym <v.trof@icloud.com>
2022-09-10 13:34:40 +0300

    added task №10

--- js/task-10.js
+++ js/task-10.js
@@ -13,6 +13,52 @@
 
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  input: document.querySelector('[type="number"]'),
+  btnCreate: document.querySelector('[data-create]'),
+  div: document.querySelector('#controls'),
+  divBoxes: document.querySelector('#boxes'),
+  btnDestroy: document.querySelector('[data-destroy]')
+}
+
+refs.btnCreate.addEventListener('click', onBtnCreateClick)
+
+function onBtnCreateClick() {
+  const amount = refs.input.value;
+  createBoxes(amount)
+  refs.input.value = '';
+}
+
+refs.btnDestroy.addEventListener('click', onBtnDestroyClick);
+function onBtnDestroyClick() {
+  const countInput = refs.input.value;
+  if (!countInput || countInput <0) {
+    refs.divBoxes.innerHTML = '';
+    return
+  }
+  const quantity = refs.divBoxes.childElementCount;
+  for (let i = 0; i < countInput; i++) {
+    if (i > quantity -1 ) {
+      return
+    }
+    refs.divBoxes.lastElementChild.remove();
+}
+}
+
+function createBoxes(amount) {
+  let size = 30;
+for (let i = 0; i < amount; i +=1) {
+  const box = document.createElement('div');
+  box.style.width = size + 'px';
+  box.style.height = size + 'px';
+  box.style.backgroundColor = getRandomHexColor();
+  refs.divBoxes.append(box);
+  size += 10;
+  }
 }
 
